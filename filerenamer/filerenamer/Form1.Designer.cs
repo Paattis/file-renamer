74d75a0fe6b9b6885a5c3979be909998a1735f88
Paattis <leevi.paattilammi@gmail.com>
2015-07-19 00:11:46 +0300

    ´

---
+++ filerenamer/filerenamer/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace filerenamer
+{
+    partial class btn_selectFiles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txt_regex = new System.Windows.Forms.TextBox();
+            this.lbl_regexPattern = new System.Windows.Forms.Label();
+            this.txt_newFilename = new System.Windows.Forms.TextBox();
+            this.lbl_newFilename = new System.Windows.Forms.Label();
+            this.txt_replaceText = new System.Windows.Forms.TextBox();
+            this.lbl_replaceWith = new System.Windows.Forms.Label();
+            this.radio_regexPattern = new System.Windows.Forms.RadioButton();
+            this.radio_newtext = new System.Windows.Forms.RadioButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btn_startRenaming = new System.Windows.Forms.Button();
+            this.progbar_renameProgress = new System.Windows.Forms.ProgressBar();
+            this.lbl_filecounter = new System.Windows.Forms.Label();
+            this.txt_selectedfiles = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txt_regex
+            // 
+            this.txt_regex.Location = new System.Drawing.Point(114, 277);
+            this.txt_regex.Name = "txt_regex";
+            this.txt_regex.Size = new System.Drawing.Size(158, 20);
+            this.txt_regex.TabIndex = 2;
+            this.txt_regex.Visible = false;
+            // 
+            // lbl_regexPattern
+            // 
+            this.lbl_regexPattern.Location = new System.Drawing.Point(18, 280);
+            this.lbl_regexPattern.Name = "lbl_regexPattern";
+            this.lbl_regexPattern.Size = new System.Drawing.Size(90, 17);
+            this.lbl_regexPattern.TabIndex = 0;
+            this.lbl_regexPattern.Text = "Regex:";
+            this.lbl_regexPattern.Visible = false;
+            // 
+            // txt_newFilename
+            // 
+            this.txt_newFilename.Location = new System.Drawing.Point(98, 253);
+            this.txt_newFilename.Name = "txt_newFilename";
+            this.txt_newFilename.Size = new System.Drawing.Size(174, 20);
+            this.txt_newFilename.TabIndex = 4;
+            this.txt_newFilename.TextChanged += new System.EventHandler(this.txt_newFilename_TextChanged);
+            // 
+            // lbl_newFilename
+            // 
+            this.lbl_newFilename.AutoSize = true;
+            this.lbl_newFilename.Location = new System.Drawing.Point(18, 256);
+            this.lbl_newFilename.Name = "lbl_newFilename";
+            this.lbl_newFilename.Size = new System.Drawing.Size(74, 13);
+            this.lbl_newFilename.TabIndex = 5;
+            this.lbl_newFilename.Text = "New filename:";
+            // 
+            // txt_replaceText
+            // 
+            this.txt_replaceText.Location = new System.Drawing.Point(114, 306);
+            this.txt_replaceText.Name = "txt_replaceText";
+            this.txt_replaceText.Size = new System.Drawing.Size(158, 20);
+            this.txt_replaceText.TabIndex = 6;
+            this.txt_replaceText.Visible = false;
+            this.txt_replaceText.TextChanged += new System.EventHandler(this.txt_replaceText_TextChanged);
+            // 
+            // lbl_replaceWith
+            // 
+            this.lbl_replaceWith.AutoSize = true;
+            this.lbl_replaceWith.Location = new System.Drawing.Point(18, 309);
+            this.lbl_replaceWith.Name = "lbl_replaceWith";
+            this.lbl_replaceWith.Size = new System.Drawing.Size(72, 13);
+            this.lbl_replaceWith.TabIndex = 7;
+            this.lbl_replaceWith.Text = "Replace with:";
+            this.lbl_replaceWith.Visible = false;
+            // 
+            // radio_regexPattern
+            // 
+            this.radio_regexPattern.AutoSize = true;
+            this.radio_regexPattern.Location = new System.Drawing.Point(21, 230);
+            this.radio_regexPattern.Name = "radio_regexPattern";
+            this.radio_regexPattern.Size = new System.Drawing.Size(130, 17);
+            this.radio_regexPattern.TabIndex = 8;
+            this.radio_regexPattern.Text = "Replace regex pattern";
+            this.radio_regexPattern.UseVisualStyleBackColor = true;
+            this.radio_regexPattern.CheckedChanged += new System.EventHandler(this.radio_regexPattern_CheckedChanged);
+            // 
+            // radio_newtext
+            // 
+            this.radio_newtext.AutoSize = true;
+            this.radio_newtext.Checked = true;
+            this.radio_newtext.Location = new System.Drawing.Point(21, 207);
+            this.radio_newtext.Name = "radio_newtext";
+            this.radio_newtext.Size = new System.Drawing.Size(203, 17);
+            this.radio_newtext.TabIndex = 9;
+            this.radio_newtext.TabStop = true;
+            this.radio_newtext.Text = "Rename every file the same + number";
+            this.radio_newtext.UseVisualStyleBackColor = true;
+            this.radio_newtext.CheckedChanged += new System.EventHandler(this.radio_newtext_CheckedChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // btn_startRenaming
+            // 
+            this.btn_startRenaming.Enabled = false;
+            this.btn_startRenaming.Location = new System.Drawing.Point(12, 325);
+            this.btn_startRenaming.Name = "btn_startRenaming";
+            this.btn_startRenaming.Size = new System.Drawing.Size(75, 23);
+            this.btn_startRenaming.TabIndex = 10;
+            this.btn_startRenaming.Text = "Start Renaming";
+            this.btn_startRenaming.UseVisualStyleBackColor = true;
+            this.btn_startRenaming.Click += new System.EventHandler(this.btn_startRenaming_Click);
+            // 
+            // progbar_renameProgress
+            // 
+            this.progbar_renameProgress.Location = new System.Drawing.Point(12, 363);
+            this.progbar_renameProgress.Name = "progbar_renameProgress";
+            this.progbar_renameProgress.Size = new System.Drawing.Size(201, 23);
+            this.progbar_renameProgress.Step = 1;
+            this.progbar_renameProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progbar_renameProgress.TabIndex = 11;
+            this.progbar_renameProgress.Visible = false;
+            // 
+            // lbl_filecounter
+            // 
+            this.lbl_filecounter.AutoSize = true;
+            this.lbl_filecounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_filecounter.Location = new System.Drawing.Point(219, 366);
+            this.lbl_filecounter.Name = "lbl_filecounter";
+            this.lbl_filecounter.Size = new System.Drawing.Size(31, 20);
+            this.lbl_filecounter.TabIndex = 12;
+            this.lbl_filecounter.Text = "0/0";
+            this.lbl_filecounter.Visible = false;
+            // 
+            // txt_selectedfiles
+            // 
+            this.txt_selectedfiles.Location = new System.Drawing.Point(12, 13);
+            this.txt_selectedfiles.Multiline = true;
+            this.txt_selectedfiles.Name = "txt_selectedfiles";
+            this.txt_selectedfiles.ReadOnly = true;
+            this.txt_selectedfiles.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt_selectedfiles.Size = new System.Drawing.Size(447, 148);
+            this.txt_selectedfiles.TabIndex = 13;
+            // 
+            // btn_selectFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 394);
+            this.Controls.Add(this.txt_selectedfiles);
+            this.Controls.Add(this.lbl_filecounter);
+            this.Controls.Add(this.progbar_renameProgress);
+            this.Controls.Add(this.btn_startRenaming);
+            this.Controls.Add(this.radio_newtext);
+            this.Controls.Add(this.radio_regexPattern);
+            this.Controls.Add(this.lbl_replaceWith);
+            this.Controls.Add(this.txt_replaceText);
+            this.Controls.Add(this.lbl_newFilename);
+            this.Controls.Add(this.txt_newFilename);
+            this.Controls.Add(this.lbl_regexPattern);
+            this.Controls.Add(this.txt_regex);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(900, 900);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "btn_selectFiles";
+            this.Text = "File renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txt_regex;
+        private System.Windows.Forms.Label lbl_regexPattern;
+        private System.Windows.Forms.TextBox txt_newFilename;
+        private System.Windows.Forms.Label lbl_newFilename;
+        private System.Windows.Forms.TextBox txt_replaceText;
+        private System.Windows.Forms.Label lbl_replaceWith;
+        private System.Windows.Forms.RadioButton radio_regexPattern;
+        private System.Windows.Forms.RadioButton radio_newtext;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btn_startRenaming;
+        private System.Windows.Forms.ProgressBar progbar_renameProgress;
+        private System.Windows.Forms.Label lbl_filecounter;
+        private System.Windows.Forms.TextBox txt_selectedfiles;
+    }
+}
+
---
+++ filerenamer/filerenamer/frm_main.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace filerenamer
+{
+    public partial class btn_selectFiles : Form
+    {
+
+        public string mode = "allsame";
+        public btn_selectFiles()
+        {
+            InitializeComponent();
+        }   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //initialize the file selection dialog
+            this.openFileDialog1.Filter = "Images (*.BMP;*.PNG;*.JPG;*.GIF)|*.BMP;*.PNG;*.JPG;*.GIF|" + "Audio files (*.MP3;*.FLAC;*.WAV)|*.MP3;*.FLAC;*.WAV|" + "All files (*.*)|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Select the files to rename";
+
+            //show the file selection dialog
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                Array files = openFileDialog1.FileNames.ToArray();
+
+                foreach (string file in files)
+                {
+                    txt_selectedfiles.AppendText(file+"\r\n");
+                }
+
+                btn_startRenaming.Enabled = true;
+            }
+            else if (dr == DialogResult.Cancel)
+            {
+                //User pressed cancel
+            }
+        }
+
+       
+                
+        private void radio_regexPattern_CheckedChanged(object sender, EventArgs e)
+        {
+            //make the radio buttons switch between the two modes
+            if (radio_regexPattern.Checked == false)
+            {
+                lbl_regexPattern.Visible = false;
+                lbl_replaceWith.Visible = false;
+
+                txt_regex.Visible = false;
+                txt_replaceText.Visible = false;
+                                
+            }
+            else
+            {
+                lbl_regexPattern.Visible = true;
+                lbl_replaceWith.Visible = true;
+
+                txt_regex.Visible = true;
+                txt_replaceText.Visible = true;
+
+                mode = "regexreplace";
+            }
+        }
+
+        private void radio_newtext_CheckedChanged(object sender, EventArgs e)
+        {
+            //make the radio buttons switch between the two modes
+            if (radio_newtext.Checked == false)
+            {
+                lbl_newFilename.Visible = false;
+                txt_newFilename.Visible = false;
+            }
+            else
+            {
+                lbl_newFilename.Visible = true;
+                txt_newFilename.Visible = true;
+
+                mode = "allsame";
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            /* rename the files */
+
+            Array files = this.openFileDialog1.FileNames.ToArray();
+            int fileamount = files.Length;
+            int filesrenamed = 0;
+
+            //report that the renaming is starting so the UI thread can make the 
+            backgroundWorker1.ReportProgress(0, "starting");
+
+            string filedirectory;
+            string newfilepath = "";
+            string filename;
+            string newfilename;
+            string extension;
+            string regexpattern = txt_regex.Text;
+            int i = 1;
+
+
+
+            foreach (string file in files)
+            {
+                filedirectory = System.IO.Path.GetDirectoryName(file);
+                filename = System.IO.Path.GetFileName(file);
+                extension = System.IO.Path.GetExtension(file);
+
+                if (mode == "allsame")
+                {
+                    newfilepath = filedirectory + "\\" + txt_newFilename.Text + "_" + i + extension;
+                    
+                }
+                else if (mode == "regexreplace")
+                {
+                    Regex rgx = new Regex(regexpattern);
+                    newfilename = rgx.Replace(filename, txt_replaceText.Text);
+                    newfilepath = filedirectory + "\\" + newfilename+extension;
+                }
+
+                System.Threading.Thread.Sleep(1000);
+
+                try
+                {
+                    System.IO.File.Move(file, newfilepath);
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("Something went wrong while renaming "+file+": "+error.ToString());
+                }
+                               
+                backgroundWorker1.ReportProgress(i);
+
+                i++;
+
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Array files = this.openFileDialog1.FileNames.ToArray();
+            int fileamount = files.Length;
+            int filesrenamed = e.ProgressPercentage;
+
+            progbar_renameProgress.Value = filesrenamed;
+            lbl_filecounter.Text = filesrenamed + "/" + fileamount;
+                      
+                       
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+            btn_startRenaming.Enabled = true;
+
+        }
+
+        private void btn_startRenaming_Click(object sender, EventArgs e)
+        {
+            Array files = this.openFileDialog1.FileNames.ToArray();
+            int fileamount = files.Length;
+            int filesrenamed = 0;
+
+            btn_startRenaming.Enabled = false;
+
+            //set the progressbar visible
+            progbar_renameProgress.Value = 0;
+            progbar_renameProgress.Maximum = fileamount;
+            progbar_renameProgress.Visible = true;
+
+
+            //set the label next to the progressbar to display the amount of renamed files and the amount of files in total
+            lbl_filecounter.Visible = true;
+            lbl_filecounter.Text = 0 + "/" + fileamount;
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+
+        //make sure the new filename won't have invalid characters
+        //(the next two events
+        private void txt_newFilename_TextChanged(object sender, EventArgs e)
+        {
+            if (IsValidFileName(txt_newFilename.Text) == false)
+            {
+                MessageBox.Show("The filename is invalid!");
+                btn_startRenaming.Enabled = false;
+            }
+            else
+            {
+                btn_startRenaming.Enabled = true;
+            }
+        }
+           
+        private void txt_replaceText_TextChanged(object sender, EventArgs e)
+        {
+            if (IsValidFileName(txt_replaceText.Text) == false)
+            {
+                MessageBox.Show("The filename is invalid!");
+                btn_startRenaming.Enabled = false;
+            }
+            else
+            {
+                btn_startRenaming.Enabled = true;
+            }
+        }
+
+        
+
+        //checks if the input is a valid windows filename
+        public static bool IsValidFileName(string expression, bool platformIndependent = false)
+        {
+            string sPattern = @"^(?!^(PRN|AUX|CLOCK\$|NUL|CON|COM\d|LPT\d|\..*)(\..+)?$)[^\x00-\x1f\\?*:\"";|/]+$";
+            if (platformIndependent)
+            {
+                sPattern = @"^(([a-zA-Z]:|\\)\\)?(((\.)|(\.\.)|([^\\/:\*\?""\|<>\. ](([^\\/:\*\?""\|<>\. ])|([^\\/:\*\?""\|<>]*[^\\/:\*\?""\|<>\. ]))?))\\)*[^\\/:\*\?""\|<>\. ](([^\\/:\*\?""\|<>\. ])|([^\\/:\*\?""\|<>]*[^\\/:\*\?""\|<>\. ]))?$";
+            }
+            return (Regex.IsMatch(expression, sPattern, RegexOptions.CultureInvariant));
+        }
+
+        
+
+        
+
+        
+
+        
+
+        
+                     
+    }
+        
+}
